e80770c73602785518c32f2516cbe279c0356c84
Daniel Racz <raczdaniel@gpu2.ilab.sztaki.hu>
2020-12-06 05:47:34 +0000

    day 6 added

---
+++ 2020/6/6.py
@@ -0,0 +1,19 @@
+from functools import reduce
+
+INPUT_FILE = "6.in"
+
+with open(INPUT_FILE, 'r') as f:
+    inp = f.read().split("\n\n")
+
+# part1
+print(sum(len(set("".join(group.strip().split("\n")))) for group in inp))
+
+# part2
+print(sum(1 for group in inp
+          for c in set(group)
+          if all(c in line for line in group.splitlines())))
+
+# part2 using reduce (reddit)
+print(sum(len(reduce(set.intersection, (set(line)
+                                        for line in group.splitlines())))
+          for group in inp))
