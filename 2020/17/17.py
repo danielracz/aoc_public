524c632ef34ff713eeff42275a9bd5a3674495f4
Daniel Racz <raczdaniel@gpu2.ilab.sztaki.hu>
2020-12-17 11:33:12 +0000

    small fixes

--- 2020/17/17.py
+++ 2020/17/17.py
@@ -5,17 +5,17 @@ INPUT_FILE = '17.in'
 with open(INPUT_FILE, "r") as f:
     grid = np.array([[c == "#" for c in line] for line in f.read().splitlines()])
 
-def solve(dim, grid):
+def solve(dim, grid, num_cycles):
     assert dim >= 2
     grid = np.pad(grid[(None,) * (dim - 2)], [(6,)] * dim)
     kernel = np.ones(shape = (3,) * dim, dtype = np.int8)
     kernel[(1,) * dim] = 0
 
-    for i in range(6):
+    for _ in range(num_cycles):
         ns = convolve(grid, kernel, mode = 'same')
         grid = (grid & np.isin(ns, [2, 3])) | (~grid & (ns == 3))
 
     return np.sum(grid)
 
-print(solve(3, grid))
-print(solve(4, grid))
+print(solve(3, grid, 6))
+print(solve(4, grid, 6))
